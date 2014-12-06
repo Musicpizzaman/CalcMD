18eef42eba6bc27901ac1b021fafae9cde99758e
Musicpizzaman <user@User.local>
2014-12-05 20:55:04 -0600

    CalcMD | A Woman's Health Calculator
    I will expand the functionality of the program to encompass more
    platforms and calculators.

---
+++ Due Date Calulator/Due Date Calulator/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalcMD
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CalcMD))
+        Me.btnCalDueDate = New System.Windows.Forms.Button()
+        Me.gbLastMP = New System.Windows.Forms.GroupBox()
+        Me.dtpLastMP = New System.Windows.Forms.DateTimePicker()
+        Me.gbAppointment = New System.Windows.Forms.GroupBox()
+        Me.dtpNextApp = New System.Windows.Forms.DateTimePicker()
+        Me.lblDisplayoutput = New System.Windows.Forms.Label()
+        Me.btnTXTOut = New System.Windows.Forms.Button()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsbtnDueDate = New System.Windows.Forms.ToolStripButton()
+        Me.tsbtnOvulation = New System.Windows.Forms.ToolStripButton()
+        Me.btnDueDate = New System.Windows.Forms.Button()
+        Me.btnOvulation = New System.Windows.Forms.Button()
+        Me.btnCalcOv = New System.Windows.Forms.Button()
+        Me.gbFirstDayCycle = New System.Windows.Forms.GroupBox()
+        Me.txtAvgLength = New System.Windows.Forms.TextBox()
+        Me.lblOvDisplay = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.gbLastMP.SuspendLayout()
+        Me.gbAppointment.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.gbFirstDayCycle.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCalDueDate
+        '
+        Me.btnCalDueDate.Location = New System.Drawing.Point(11, 245)
+        Me.btnCalDueDate.Name = "btnCalDueDate"
+        Me.btnCalDueDate.Size = New System.Drawing.Size(216, 44)
+        Me.btnCalDueDate.TabIndex = 0
+        Me.btnCalDueDate.Text = "Calculate"
+        Me.btnCalDueDate.UseVisualStyleBackColor = True
+        '
+        'gbLastMP
+        '
+        Me.gbLastMP.Controls.Add(Me.dtpLastMP)
+        Me.gbLastMP.Location = New System.Drawing.Point(12, 27)
+        Me.gbLastMP.Name = "gbLastMP"
+        Me.gbLastMP.Size = New System.Drawing.Size(215, 60)
+        Me.gbLastMP.TabIndex = 1
+        Me.gbLastMP.TabStop = False
+        Me.gbLastMP.Text = "Last Menstral Period"
+        '
+        'dtpLastMP
+        '
+        Me.dtpLastMP.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpLastMP.Location = New System.Drawing.Point(6, 24)
+        Me.dtpLastMP.Name = "dtpLastMP"
+        Me.dtpLastMP.Size = New System.Drawing.Size(200, 20)
+        Me.dtpLastMP.TabIndex = 5
+        Me.dtpLastMP.Value = New Date(2014, 9, 8, 0, 28, 39, 0)
+        '
+        'gbAppointment
+        '
+        Me.gbAppointment.Controls.Add(Me.dtpNextApp)
+        Me.gbAppointment.Location = New System.Drawing.Point(233, 27)
+        Me.gbAppointment.Name = "gbAppointment"
+        Me.gbAppointment.Size = New System.Drawing.Size(214, 60)
+        Me.gbAppointment.TabIndex = 2
+        Me.gbAppointment.TabStop = False
+        Me.gbAppointment.Text = "Appointment Date"
+        '
+        'dtpNextApp
+        '
+        Me.dtpNextApp.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpNextApp.Location = New System.Drawing.Point(7, 23)
+        Me.dtpNextApp.Name = "dtpNextApp"
+        Me.dtpNextApp.Size = New System.Drawing.Size(200, 20)
+        Me.dtpNextApp.TabIndex = 0
+        '
+        'lblDisplayoutput
+        '
+        Me.lblDisplayoutput.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblDisplayoutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDisplayoutput.Location = New System.Drawing.Point(12, 90)
+        Me.lblDisplayoutput.Name = "lblDisplayoutput"
+        Me.lblDisplayoutput.Size = New System.Drawing.Size(435, 152)
+        Me.lblDisplayoutput.TabIndex = 3
+        '
+        'btnTXTOut
+        '
+        Me.btnTXTOut.Location = New System.Drawing.Point(233, 246)
+        Me.btnTXTOut.Name = "btnTXTOut"
+        Me.btnTXTOut.Size = New System.Drawing.Size(214, 43)
+        Me.btnTXTOut.TabIndex = 4
+        Me.btnTXTOut.Text = "Print To Text File"
+        Me.btnTXTOut.UseVisualStyleBackColor = True
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbtnDueDate, Me.tsbtnOvulation})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(456, 25)
+        Me.ToolStrip1.TabIndex = 8
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbtnDueDate
+        '
+        Me.tsbtnDueDate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbtnDueDate.Image = CType(resources.GetObject("tsbtnDueDate.Image"), System.Drawing.Image)
+        Me.tsbtnDueDate.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtnDueDate.Name = "tsbtnDueDate"
+        Me.tsbtnDueDate.Size = New System.Drawing.Size(59, 22)
+        Me.tsbtnDueDate.Text = "Due Date"
+        '
+        'tsbtnOvulation
+        '
+        Me.tsbtnOvulation.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbtnOvulation.Image = CType(resources.GetObject("tsbtnOvulation.Image"), System.Drawing.Image)
+        Me.tsbtnOvulation.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtnOvulation.Name = "tsbtnOvulation"
+        Me.tsbtnOvulation.Size = New System.Drawing.Size(63, 22)
+        Me.tsbtnOvulation.Text = "Ovulation"
+        '
+        'btnDueDate
+        '
+        Me.btnDueDate.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnDueDate.Location = New System.Drawing.Point(0, 25)
+        Me.btnDueDate.Name = "btnDueDate"
+        Me.btnDueDate.Size = New System.Drawing.Size(456, 161)
+        Me.btnDueDate.TabIndex = 9
+        Me.btnDueDate.Text = "Calculate Due Date"
+        Me.btnDueDate.UseVisualStyleBackColor = True
+        '
+        'btnOvulation
+        '
+        Me.btnOvulation.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnOvulation.Location = New System.Drawing.Point(0, 154)
+        Me.btnOvulation.Name = "btnOvulation"
+        Me.btnOvulation.Size = New System.Drawing.Size(456, 148)
+        Me.btnOvulation.TabIndex = 10
+        Me.btnOvulation.Text = "Calculate Ovulation"
+        Me.btnOvulation.UseVisualStyleBackColor = True
+        '
+        'btnCalcOv
+        '
+        Me.btnCalcOv.Location = New System.Drawing.Point(12, 246)
+        Me.btnCalcOv.Name = "btnCalcOv"
+        Me.btnCalcOv.Size = New System.Drawing.Size(435, 44)
+        Me.btnCalcOv.TabIndex = 11
+        Me.btnCalcOv.Text = "Calculate"
+        Me.btnCalcOv.UseVisualStyleBackColor = True
+        '
+        'gbFirstDayCycle
+        '
+        Me.gbFirstDayCycle.Controls.Add(Me.txtAvgLength)
+        Me.gbFirstDayCycle.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.gbFirstDayCycle.Location = New System.Drawing.Point(12, 28)
+        Me.gbFirstDayCycle.Name = "gbFirstDayCycle"
+        Me.gbFirstDayCycle.Size = New System.Drawing.Size(215, 199)
+        Me.gbFirstDayCycle.TabIndex = 6
+        Me.gbFirstDayCycle.TabStop = False
+        Me.gbFirstDayCycle.Text = "Average Length of Cycle "
+        '
+        'txtAvgLength
+        '
+        Me.txtAvgLength.Font = New System.Drawing.Font("Microsoft Sans Serif", 112.0!, System.Drawing.FontStyle.Bold)
+        Me.txtAvgLength.Location = New System.Drawing.Point(6, 18)
+        Me.txtAvgLength.Name = "txtAvgLength"
+        Me.txtAvgLength.Size = New System.Drawing.Size(203, 177)
+        Me.txtAvgLength.TabIndex = 0
+        Me.txtAvgLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblOvDisplay
+        '
+        Me.lblOvDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblOvDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOvDisplay.Location = New System.Drawing.Point(233, 28)
+        Me.lblOvDisplay.Name = "lblOvDisplay"
+        Me.lblOvDisplay.Size = New System.Drawing.Size(213, 199)
+        Me.lblOvDisplay.TabIndex = 12
+        '
+        'CalcMD
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(456, 302)
+        Me.Controls.Add(Me.lblOvDisplay)
+        Me.Controls.Add(Me.gbFirstDayCycle)
+        Me.Controls.Add(Me.btnCalcOv)
+        Me.Controls.Add(Me.btnTXTOut)
+        Me.Controls.Add(Me.lblDisplayoutput)
+        Me.Controls.Add(Me.gbAppointment)
+        Me.Controls.Add(Me.gbLastMP)
+        Me.Controls.Add(Me.btnCalDueDate)
+        Me.Controls.Add(Me.btnDueDate)
+        Me.Controls.Add(Me.btnOvulation)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "CalcMD"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.gbLastMP.ResumeLayout(False)
+        Me.gbAppointment.ResumeLayout(False)
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.gbFirstDayCycle.ResumeLayout(False)
+        Me.gbFirstDayCycle.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCalDueDate As System.Windows.Forms.Button
+    Friend WithEvents gbLastMP As System.Windows.Forms.GroupBox
+    Friend WithEvents gbAppointment As System.Windows.Forms.GroupBox
+    Friend WithEvents lblDisplayoutput As System.Windows.Forms.Label
+    Friend WithEvents dtpLastMP As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dtpNextApp As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnTXTOut As System.Windows.Forms.Button
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents tsbtnDueDate As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbtnOvulation As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btnDueDate As System.Windows.Forms.Button
+    Friend WithEvents btnOvulation As System.Windows.Forms.Button
+    Friend WithEvents btnCalcOv As System.Windows.Forms.Button
+    Friend WithEvents gbFirstDayCycle As System.Windows.Forms.GroupBox
+    Friend WithEvents lblOvDisplay As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents txtAvgLength As System.Windows.Forms.TextBox
+
+End Class
---
+++ Due Date Calulator/Due Date Calulator/Form1.vb
@@ -0,0 +1,256 @@
+﻿Imports System.Globalization
+Public Class CalcMD
+
+    'Author : David Otero
+    'Product Discription : This woman's health calculator is a high fidelity prototype
+    'This prototype is close enough to the final product, in terms of detail and functionality
+    'Examining usability functionality is built in
+
+    Dim todaysDate As String = String.Format("{0:dd-MM-yyyy}", DateTime.Now)
+
+    'Testing File creation 
+
+    Dim csvFile As String = My.Application.Info.DirectoryPath & "\004 Test Results " + todaysDate + ".csv"
+    'Dim outFile As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(csvFile, True)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim outFile As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(csvFile, True)
+        'Form load time stamp to csv file
+        outFile.WriteLine("TEST 5")
+        outFile.WriteLine("Form Loaded" + "," + TimeOfDay)
+        outFile.Close()
+
+        ' on form load and interface elements are disabled
+        ToolStrip1.Visible = False
+        tsbtnDueDate.Visible = False
+        tsbtnOvulation.Visible = False
+        gbLastMP.Visible = False
+        gbAppointment.Visible = False
+        dtpLastMP.Visible = False
+        dtpNextApp.Visible = False
+        lblDisplayoutput.Visible = False
+        btnCalDueDate.Visible = False
+        btnTXTOut.Visible = False
+        btnCalcOv.Visible = False
+        gbFirstDayCycle.Visible = False
+        txtAvgLength.Visible = False
+        lblOvDisplay.Visible = False
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalDueDate.Click
+        Dim outFile As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(csvFile, True)
+        'time stamp to csv file
+        outFile.WriteLine("btnCalDueDate" + "," + TimeOfDay)
+        outFile.Close()
+
+        Dim CurrentDay As Date
+        Dim birthday As Date
+        Dim Weeks As Double
+        Dim days As String
+        Dim test As Integer
+
+
+        CurrentDay = dtpLastMP.Value.ToShortDateString
+        birthday = dtpLastMP.Value.AddDays(280).ToShortDateString
+        days = dtpNextApp.Value.Date.Subtract _
+            (dtpLastMP.Value.Date).Days
+
+        'week conversion
+        Weeks = CDbl(days / 7)
+        Weeks.ToString("F2", CultureInfo.InvariantCulture)
+
+        'one day is 0.142857 of a week
+
+        'whole number weeks with days conversion 
+        If (Integer.TryParse(Weeks, test)) Then
+            lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks."
+        Else
+
+            'week & 1 day
+            Weeks = (CDbl(days / 7) - 0.14285714285714285)
+            If (Integer.TryParse(Weeks, test)) Then
+                lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 1 day"
+            Else
+
+                'week & 2 day
+                Weeks = (CDbl(days / 7) - 0.2857142857142857)
+                If (Integer.TryParse(Weeks, test)) Then
+                    lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 2 days"
+                Else
+
+                    'week & 3 day
+                    Weeks = (CDbl(days / 7) - 0.42857142857142855)
+                    If (Integer.TryParse(Weeks, test)) Then
+                        lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 3 days"
+                    Else
+
+                        'week & 4 day
+                        Weeks = (CDbl(days / 7) - 0.5714285714285714)
+                        If (Integer.TryParse(Weeks, test)) Then
+                            lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 4 days"
+                        Else
+
+                            'week & 5 day
+                            Weeks = (CDbl(days / 7) - 0.7142857142857143)
+                            If (Integer.TryParse(Weeks, test)) Then
+                                lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 5 days"
+                            Else
+
+                                'week & 6 day
+                                Weeks = (CDbl(days / 7) - 0.8571428571428571)
+                                If (Integer.TryParse(Weeks, test)) Then
+                                    lblDisplayoutput.Text = "Your due date is approximately: " + birthday & Environment.NewLine & "You have been prego for " + CStr(Weeks) + " weeks and 6 days"
+
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles tsbtnDueDate.Click
+
+        tsbtnDueDate.BackColor = Color.Azure
+        'Form interface change
+        gbLastMP.Visible = True
+        gbAppointment.Visible = True
+        dtpLastMP.Visible = True
+        dtpNextApp.Visible = True
+        lblDisplayoutput.Visible = True
+        btnCalDueDate.Visible = True
+        btnTXTOut.Visible = True
+        btnCalcOv.Visible = False
+        gbFirstDayCycle.Visible = False
+        txtAvgLength.Visible = False
+        lblOvDisplay.Visible = False
+    End Sub
+
+    Private Sub btnPDFOut_Click(sender As Object, e As EventArgs) Handles btnTXTOut.Click
+        Dim outFile As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(csvFile, True)
+        'time stamp to csv file
+        outFile.WriteLine("btnTXTOut" + "," + TimeOfDay)
+        outFile.Close()
+
+        'output the data to txt file
+        Dim path As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Appointment.txt"
+        Dim writer As New System.IO.StreamWriter(path, True)
+        writer.WriteLine(lblDisplayoutput.Text)
+        writer.Close()
+
+        Dim msg = "File has been saved to the MyDocuments Folder"
+        Dim style = MsgBoxStyle.OkOnly
+
+        ' Display the message box and save the response, Yes or No. 
+        Dim response = MsgBox(msg, style)
+
+        lblDisplayoutput.Text = ""
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnDueDate.Click
+
+        'Form interface change
+        ToolStrip1.Visible = True
+        tsbtnDueDate.Visible = True
+        tsbtnOvulation.Visible = True
+        gbLastMP.Visible = True
+        gbAppointment.Visible = True
+        dtpLastMP.Visible = True
+        dtpNextApp.Visible = True
+        lblDisplayoutput.Visible = True
+        btnCalDueDate.Visible = True
+        btnTXTOut.Visible = True
+        btnDueDate.Visible = False
+        btnOvulation.Visible = False
+        btnCalcOv.Visible = False
+        gbFirstDayCycle.Visible = False
+        txtAvgLength.Visible = False
+        lblOvDisplay.Visible = False
+
+    End Sub
+
+    Private Sub btnOvulation_Click(sender As Object, e As EventArgs) Handles btnOvulation.Click
+
+        'Form interface change
+        btnDueDate.Visible = False
+        btnOvulation.Visible = False
+        gbLastMP.Visible = False
+        gbAppointment.Visible = False
+        dtpLastMP.Visible = False
+        dtpNextApp.Visible = False
+        lblDisplayoutput.Visible = False
+        btnCalDueDate.Visible = False
+        btnTXTOut.Visible = False
+
+        ToolStrip1.Visible = True
+        tsbtnDueDate.Visible = True
+        tsbtnOvulation.Visible = True
+        btnCalcOv.Visible = True
+        gbFirstDayCycle.Visible = True
+        txtAvgLength.Visible = True
+        lblOvDisplay.Visible = True
+
+    End Sub
+
+    Private Sub tsbtnOvulation_Click(sender As Object, e As EventArgs) Handles tsbtnOvulation.Click
+
+        tsbtnOvulation.BackColor = Color.Azure
+
+        'Form interface change
+        btnDueDate.Visible = False
+        btnOvulation.Visible = False
+        gbLastMP.Visible = False
+        gbAppointment.Visible = False
+        dtpLastMP.Visible = False
+        dtpNextApp.Visible = False
+        lblDisplayoutput.Visible = False
+        btnCalDueDate.Visible = False
+        btnTXTOut.Visible = False
+
+        ToolStrip1.Visible = True
+        tsbtnDueDate.Visible = True
+        tsbtnOvulation.Visible = True
+        btnCalcOv.Visible = True
+        gbFirstDayCycle.Visible = True
+        txtAvgLength.Visible = True
+        lblOvDisplay.Visible = True
+
+    End Sub
+
+    Private Sub btnCalcOv_Click(sender As Object, e As EventArgs) Handles btnCalcOv.Click
+        Dim outFile As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(csvFile, True)
+        'time stamp to csv file
+        outFile.WriteLine("btnCalcOv" + "," + TimeOfDay)
+        outFile.Close()
+
+        If String.IsNullOrEmpty(txtAvgLength.Text) Then
+            'do nothing if input is empty
+        Else
+
+            Dim AvgCycle As Integer = txtAvgLength.Text
+            Dim FollicularPhase As Integer = 14
+            Dim Ovulation As Integer
+
+
+            If AvgCycle <= 20 Or AvgCycle >= 38 Then
+                Dim msg = "A normal menstrual cycle lasts for 22 to 36 days contact a Medical Professional if you are outside of those bounds"
+                Dim style = MsgBoxStyle.OkOnly
+
+                ' Display the message box and save the response, Yes or No. 
+                Dim response = MsgBox(msg, style)
+
+                txtAvgLength.Text = ""
+
+            Else
+                Ovulation = AvgCycle - FollicularPhase
+
+                lblOvDisplay.Text = "Your day of ovulation is aproxamately within " + CStr(Ovulation) + " plus and minus 1 days of your cycle start day"
+            End If
+        End If
+    End Sub
+
+End Class
